e48d39f96de78b3e2c54bb226d27c0bde1ca7ca7
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-03-11 20:22:32 -0300

    create database

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,11 +1,18 @@
-class UserController{
+const AppError = require('../utils/AppError')
+class UserController {
+  create(request, response) {
+    const { name, email, password } = request.body
 
-  create(request, response){
-    const {name, email, password} = request.body
+    if (!name) {
+      throw new AppError('Nome obrigatório.')
+    }
+
+    if (!email) {
+      throw new AppError('Email obrigatório.')
+    }
 
     response.json({ name, email, password })
   }
-
 }
 
 module.exports = UserController
