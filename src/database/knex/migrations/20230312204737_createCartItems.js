4f4ff86479807ea9d808b70452884a5ba4c1b5a4
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-03-12 18:08:21 -0300

    create table CartItems

--- src/database/knex/migrations/20230312204737_createCartItems.js
+++ src/database/knex/migrations/20230312204737_createCartItems.js
@@ -1,15 +1,12 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
+exports.up = knex => knex.schema.createTable("cartItems", table => {
+  table.increments("id")
+  table.integer("cart_id").references("id").inTable("carts").onDelete("CASCADE");
+  table.integer("dish_id").references("id").inTable("dishes")
   
-};
+  table.text("title")
+  table.integer("quantity")
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+  table.timestamp("created_at").default(knex.fn.now())
+})
+
+exports.down = knex => knex.schema.dropTable("cartItems")
