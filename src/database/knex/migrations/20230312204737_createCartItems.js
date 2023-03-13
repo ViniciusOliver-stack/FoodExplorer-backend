dfdacc0027c72233a1a5a0d85b6b9555ec2ec946
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-03-13 19:19:38 -0300

    createCartItems

--- src/database/knex/migrations/20230312204737_createCartItems.js
+++ src/database/knex/migrations/20230312204737_createCartItems.js
@@ -7,6 +7,7 @@ exports.up = knex => knex.schema.createTable("cartItems", table => {
   table.integer("quantity")
 
   table.timestamp("created_at").default(knex.fn.now())
+  
 })
 
 exports.down = knex => knex.schema.dropTable("cartItems")
