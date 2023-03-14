252fdc7e9af87e386f5b679912593202fcde86b5
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-03-13 22:09:47 -0300

    create method show cart

--- src/controllers/CartsController.js
+++ src/controllers/CartsController.js
@@ -25,48 +25,56 @@ class CartsController {
     return response.status(201).json();
   }
 
-  async update(request, response) {
-    const { id, status } = request.body;
+  async show(request, response){
+    const { id } = request.params
 
-    await knex("carts").update({status}).where({ id })
+    const cart = await knex('carts').where({ id }).first()
+
+    return response.json(cart)
+  }
+
+  // async update(request, response) {
+  //   const { id, status } = request.body;
+
+  //   await knex("carts").update({status}).where({ id })
     
-    return response.status(201).json();
-  }
+  //   return response.status(201).json();
+  // }
 
-  async index(request, response) {
-    const allCarts = await knex("carts")
-    const orders = await knex("cartItems")  
+  // async index(request, response) {
+  //   const allCarts = await knex("carts")
+  //   const orders = await knex("cartItems")  
 
-    const cartsWithOrders = allCarts.map(cart => {
-      const cartOrder = orders.filter(order => order.cart_id === cart.id)
+  //   const cartsWithOrders = allCarts.map(cart => {
+  //     const cartOrder = orders.filter(order => order.cart_id === cart.id)
 
-      return {
-        ...cart,
-        orders: cartOrder
-      }
-    })
-    return response.status(201).json(cartsWithOrders)
-  }
+  //     return {
+  //       ...cart,
+  //       orders: cartOrder
+  //     }
+  //   })
+  //   return response.status(201).json(cartsWithOrders)
+  // }
 
-  async show(request, response) {
-    const { id } = request.params;
+  // async show(request, response) {
+  //   const { id } = request.params;
 
-    const cart = await knex("carts").where({ user_id: id }).first();
-    const orders = await knex("cartItems").where({ cart_id: id})
+  //   const cart = await knex("carts").where({ user_id: id }).first();
+  //   const orders = await knex("cartItems").where({ cart_id: id})
 
-    return response.status(201).json({
-      ...cart,
-      orders,
-    });
-  }
+  //   return response.status(201).json({
+  //     ...cart,
+  //     orders,
+  //   });
+  // }
 
-  async delete(request, response) {
-    const { id } = request.params;
+  // async delete(request, response) {
+  //   const { id } = request.params;
 
-    await knex("carts").where({ user_id: id }).delete();
+  //   await knex("carts").where({ user_id: id }).delete();
 
-    return response.status(201).json();
-  }
+  //   return response.status(201).json();
+  // }
 }
 
 module.exports = CartsController;
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -7,6 +7,7 @@ const cartsRoutes = Router();
 const cartsController = new CartsController();
 
 cartsRoutes.post("/:user_id", cartsController.create);
+cartsRoutes.get('/:id', cartsController.show)
 
 
 module.exports = cartsRoutes;
